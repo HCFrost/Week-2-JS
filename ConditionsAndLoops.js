60a588756445540e52adb383f8812f5aa54b4496
Hunter Frost <hunter2996@gmail.com>
2021-11-18 17:23:36 -0700

    initial commit

---
+++ ConditionsAndLoops.js
@@ -0,0 +1,124 @@
+// create a variable called speedLimit and another called mySpeed
+// set their values to numbers between 1 and 100
+
+let speedLimit = 45;
+let mySpeed = 45;
+
+// using a conditional, determine if mySpeed is greater than the speedLimit
+// if true, print "Slow Down! Mom is mad!" to the console
+// if mySpeed is equal to the speedLimit, print "Everyone is happy!" to the console
+// if mySpeed is less than the speedLimit, print "Speed up! Dad is mad!"
+if (mySpeed > speedLimit){
+    console.log("HELL YEAH BROTHER!")
+}if (mySpeed == speedLimit){
+    console.log("FLOOR IT!")
+}if (mySpeed < speedLimit){
+    console.log("GET OUT OF THE DRIVERS SEAT AND LET ME DRIVE!")
+}
+
+//  My example:
+let myKills = 100;
+let myDeaths = 100;
+let myKDA = myKills / myDeaths;
+let kda = myKDA.toFixed(2)
+if (myKills > myDeaths){
+     console.log("MY KD IS " + kda + ". " + "GET PWN'D NERDS")
+}
+ if(myKills < myDeaths){
+    console.log("MY KD IS " + kda + ". " + "GET GOOD KID!")
+}
+ if(myKills == myDeaths){
+     console.log("MY KD IS " + kda + ". " + "MY KD IS EVEN!")
+}
+
+    
+        
+// create two variables, one named alarmSet, the other openDoor
+// set them to a boolean value
+let alarmSet = true;
+let openDoor = true;
+
+
+
+// using a conditional, determine if alarm is set. 
+// if alarm is set and door is set to open, print "Sound Alarm!" to the console
+// otherwise, print "Everything is fine." to the console.
+if (alarmSet == true && openDoor == true){
+    console.log("Sound Alarm!");
+}else (console.log("Everything is fine."))
+    
+// My example:
+
+
+
+// create two variables, username and password
+// create a conditional, if the username is "Tommy123" and the password is "12345"
+// or the username is "Timmy456" and the password is "6789", print "Admin Login Successful" to the console
+// otherwise, print "Admin Access Denied"
+
+let username = "HCFrost";
+let password = "OpenSeasame123";
+
+if (  username === "HCFrost"  && password === "OpenSeasame123"){
+    console.log("Welcome, " + username + "!")
+}else (console.log("Wrong username or password."))
+
+
+
+// write code that will set the value of studentClass based on studentGrade
+// studentGrade will be K-12
+// K-6 will be Elementary
+// 7-8 will be 
+// 9 will be Freshman, 10 Sophomore, 11 Junior, 12 Senior
+// Anything other than these values will return "Error" to the console
+
+let studentGrade = "k";
+let studentClass = studentGrade;
+
+if (studentGrade === "K"|| "k"){
+    console.log("Kindergarden")
+}
+else if (studentGrade <= 6){
+    console.log("Elementary")
+} else if(studentGrade > 6 && studentGrade <= 8){
+    console.log("Middle")
+} else if (studentGrade === 9){
+    console.log("Freshman")
+} else if (studentGrade === 10){
+    console.log("Sophomore")
+} else if (studentGrade === 11){
+    console.log("Junior")
+} else if (studentGrade === 12){
+    console.log("Senior")
+}else (console.log("error"))
+
+
+
+// write a for loop that will iterate backwards from 10 to -10
+//for ( i = 10 ; i >= -10; i--){
+//    console.log(i);
+//}
+
+
+// write a do/while loop that prints 1 through 50
+//let j = 1 ;
+//do( console.log(j++ % 4 ))
+//    while (j <= 50)  
+    
+
+
+// edit the previous do/while loop so that it prints the remainder when the loop number is divided by 4
+
+ 
+// Someone messed up the following for loop
+// fix the following infinite loop, uncomment to test
+ 
+//   for(let i = 15; i >= 10; i--){
+//        console.log(i);
+//    } 
+
+
+
+
+
+
